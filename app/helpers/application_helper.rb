2d9b70d6327bc7fe3cfa8d6c5457bda533358908
Leonardo Prado <leonardoprado@gmail.com>
2016-03-01 14:38:16 -0300

    Define initial layout structure

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+
+  def page_title(separator: '|', use_html: true)
+    title = t("#{controller_name}.title.#{action_name}", default: '')
+    subtitle = t("#{controller_name}.subtitle.#{action_name}", default: '')
+
+    return t('layout.site_name') if title.empty?
+    return title if subtitle.empty?
+
+    if use_html
+      "#{title} " + content_tag(:small, subtitle)
+    else
+      "#{title} #{separator} #{subtitle}"
+    end
+  end
+
+  def navbar_link(path, name, controller)
+    if current_page? controller: controller, action: action_name
+      li_options = { class: 'active' }
+      sr_tag = content_tag(:span, '(current)', class: 'sr-only')
+    end
+
+    content_tag(:li, link_to("#{name} #{sr_tag}".html_safe, path), li_options)
+  end
 end
